6d80d0f023270911fd7f42fb82f6abf68d3d1031
Sandeep Chandran <it@purehome-re.ae>
2016-12-17 14:49:55 +0400

    First Commit

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+	         
+	 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">   
+		<link rel="stylesheet" href="assets/css/bootstrap.css">
+		<title>Pure Home Real Estate - Home Page </title>
+	</head>
+	<body id="home">
+
+		<!-- Navbar Section -->
+		<nav class="navbar navbar-default">
+	  	<div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="index.php"><img src="assets/images/logo_purehome.png" alt="Pure Home Real Estate Logo"></a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">About</a></li>
+	        <!-- <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">One more separated link</a></li>
+	          </ul>
+	        </li> -->
+	        <li><a href="#">Services</a></li>
+	        <li><a href="#">Properties</a></li>
+	        <li><a href="#">Submit Property</a></li>
+	        <li><a href="#">Contact Property</a></li>
+	      </ul>
+	      <form class="navbar-form navbar-left">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Search">
+	        </div>
+	        <button type="submit" class="btn btn-default">Submit</button>
+	      </form>
+	      <!-- <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">Link</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	          </ul>
+	        </li>
+	      </ul> -->
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+	<!-- Navbar Section Ends -->
+
+
+	<!-- Main Content -->
+		<!-- Carousel -->
+		<div class="container-fluid">
+			<div class="row">
+				
+				<div id="carousel-main" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-main" data-slide-to="0" class="active"></li>
+				    <!-- <li data-target="#carousel-main" data-slide-to="1"></li>
+				    <li data-target="#carousel-main" data-slide-to="2"></li> -->
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="assets/images/carousel/abu-dhabi-slide-1.jpg" alt="Apartments in Abu Dhabi">
+				      <div class="carousel-caption">
+				        
+				      </div>
+				    </div>
+				   <!--  <div class="item">
+				      <img src="..." alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div> -->
+				 
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+				
+			</div>
+		</div>
+		<!-- Carousel Ends -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 headings-main">
+				<h1>Recent Properties</h1>
+			</div>
+			<div class="col-md-4">
+				<div class="card-property">
+					<a href="#"><img src="assets/images/properties/property1-tn.jpg" alt=""></a>
+					<h2 class="title">Hydra Avenue, Reem Island</h2>
+					<h2 class="price">AED 90,000</h2>
+					<div class="row">
+						<div class="col-md-4">
+							<h3 class="specs">1200 sq.ft.</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">4 bedrooms</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">2 bathrooms</h3>
+						</div>
+					</div>
+					<p class="description">
+						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+					</p>
+					
+					
+				</div>
+				
+			</div>
+			<div class="col-md-4">
+				<div class="card-property">
+					<a href="#"><img src="assets/images/properties/property2-tn.jpg" alt=""></a>
+					<h2 class="title">Hydra Avenue, Reem Island</h2>
+					<h2 class="price">AED 100,000</h2>
+					<div class="row">
+						<div class="col-md-4">
+							<h3 class="specs">1200 sq.ft.</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">4 bedrooms</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">2 bathrooms</h3>
+						</div>
+					</div>
+					<p class="description">
+						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card-property">
+					<a href="#"><img src="assets/images/properties/property3-tn.jpg" alt=""></a>
+					<h2 class="title">Hydra Avenue, Reem Island</h2>
+					<h2 class="price">AED 110,000</h2>
+					<div class="row">
+						<div class="col-md-4">
+							<h3 class="specs">1200 sq.ft.</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">4 bedrooms</h3>
+						</div>
+						<div class="col-md-4">
+							<h3 class="specs">2 bathrooms</h3>
+						</div>
+					</div>
+					<p class="description">
+						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Main Content Ends -->
+
+	<!-- Footer -->
+	<footer class="container-fluid">
+		<div class="row contact headings-main">
+			<h1>Get in Touch</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Footer</h1>
+			</div>
+		</div>
+	</footer>
+	<!-- Footer Ends -->
+		
+	</body>
+	<script src="assets/js/bootstrap.js"></script>
+</html>
