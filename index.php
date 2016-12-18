1f7e0dd85500e3af9872c913524fec21347b48ae
Sandeep Chandran <it@purehome-re.ae>
2016-12-18 17:59:12 +0400

    Dec 18

--- index.php
+++ index.php
@@ -7,78 +7,93 @@
 		<meta name="description" content="">
 	         
 	 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">   
+	 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+	 	<script src="https://use.fontawesome.com/06a6b6e295.js"></script>
 		<link rel="stylesheet" href="assets/css/bootstrap.css">
 		<title>Pure Home Real Estate - Home Page </title>
 	</head>
 	<body id="home">
 
 		<!-- Navbar Section -->
-		<nav class="navbar navbar-default">
+		<nav class="navbar navbar-default navbar-fixed-top ">
 	  	<div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="index.php"><img src="assets/images/logo_purehome.png" alt="Pure Home Real Estate Logo"></a>
-	    </div>
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="index.php"><img src="assets/images/logo_purehome.png" alt="Pure Home Real Estate Logo"></a>
+		    </div>
 
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-	        <li><a href="#">About</a></li>
-	        <!-- <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Action</a></li>
-	            <li><a href="#">Another action</a></li>
-	            <li><a href="#">Something else here</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="#">Separated link</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="#">One more separated link</a></li>
-	          </ul>
-	        </li> -->
-	        <li><a href="#">Services</a></li>
-	        <li><a href="#">Properties</a></li>
-	        <li><a href="#">Submit Property</a></li>
-	        <li><a href="#">Contact Property</a></li>
-	      </ul>
-	      <form class="navbar-form navbar-left">
-	        <div class="form-group">
-	          <input type="text" class="form-control" placeholder="Search">
-	        </div>
-	        <button type="submit" class="btn btn-default">Submit</button>
-	      </form>
-	      <!-- <ul class="nav navbar-nav navbar-right">
-	        <li><a href="#">Link</a></li>
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Action</a></li>
-	            <li><a href="#">Another action</a></li>
-	            <li><a href="#">Something else here</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="#">Separated link</a></li>
-	          </ul>
-	        </li>
-	      </ul> -->
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+		        <li><a href="#">About</a></li>
+		        <!-- <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li role="separator" class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		            <li role="separator" class="divider"></li>
+		            <li><a href="#">One more separated link</a></li>
+		          </ul>
+		        </li> -->
+		        <li><a href="#">Services</a></li>
+		        <li><a href="#">Properties</a></li>
+		        <li><a href="#">Submit Property</a></li>
+		        <li><a href="#">Contact</a></li>
+		      </ul>
+		      <form class="navbar-form navbar-left">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default">Submit</button>
+		      </form>
+		      <!-- <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Link</a></li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li role="separator" class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		          </ul>
+		        </li>
+		      </ul> -->
+		    </div><!-- /.navbar-collapse -->
+	  	</div>	
+		</nav>
+		<!-- Navbar Section Ends -->
 
-	<!-- Navbar Section Ends -->
+		<div class="clearfix"></div>
 
+		<!-- Contact Info Bar -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-offset-6 col-md-6 contact-info-bar ">
+					<p class="contact-item pull-right"><span class="fa fa-envelope" aria-hidden="true"></span>&nbsp; info@purehome-re.ae</p>
+					<p class="contact-item pull-right"><span class="fa fa-phone fa-lg" aria-hidden="true"></span>&nbsp; 02 4466 775</p>
+					<p class="contact-item pull-right"><span class="fa fa-mobile fa-lg" aria-hidden="true"></span>&nbsp; 055 808 2380</p>
+				</div>
+			</div>
+		</div>
 
-	<!-- Main Content -->
+		<!-- Contact Info Bar Ends -->
+	
+
+		<!-- Main Content -->
 		<!-- Carousel -->
 		<div class="container-fluid">
 			<div class="row">
-				
 				<div id="carousel-main" class="carousel slide" data-ride="carousel">
 				  <!-- Indicators -->
 				  <ol class="carousel-indicators">
@@ -86,6 +101,17 @@
 				    <!-- <li data-target="#carousel-main" data-slide-to="1"></li>
 				    <li data-target="#carousel-main" data-slide-to="2"></li> -->
 				  </ol>
+
+				  <div class="carousel-jumbotron">
+				  	<h1 class="headings-main">Professional realtor works for you</h1>
+				  	<div class="input-group">
+			      <div class="input-group-addon btn-info"><i class="fa fa-location-arrow"></i></div>
+			      <input type="text" class="form-control input-lg" id="newletter-email" placeholder="Enter Location">
+			      <span class="input-group-btn">
+	        		<button class="btn btn-warning btn-lg" type="button">SEARCH</button>
+	      		</span>
+			    </div>
+				  </div>
 
 				  <!-- Wrapper for slides -->
 				  <div class="carousel-inner" role="listbox">
@@ -101,9 +127,7 @@
 				        ...
 				      </div>
 				    </div> -->
-				 
 				  </div>
-
 				  <!-- Controls -->
 				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -114,99 +138,222 @@
 				    <span class="sr-only">Next</span>
 				  </a>
 				</div>
-				
 			</div>
 		</div>
 		<!-- Carousel Ends -->
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 headings-main">
-				<h1>Recent Properties</h1>
-			</div>
-			<div class="col-md-4">
-				<div class="card-property">
-					<a href="#"><img src="assets/images/properties/property1-tn.jpg" alt=""></a>
-					<h2 class="title">Hydra Avenue, Reem Island</h2>
-					<h2 class="price">AED 90,000</h2>
-					<div class="row">
-						<div class="col-md-4">
-							<h3 class="specs">1200 sq.ft.</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">4 bedrooms</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">2 bathrooms</h3>
-						</div>
-					</div>
-					<p class="description">
-						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
-					</p>
-					
-					
-				</div>
-				
-			</div>
-			<div class="col-md-4">
-				<div class="card-property">
-					<a href="#"><img src="assets/images/properties/property2-tn.jpg" alt=""></a>
-					<h2 class="title">Hydra Avenue, Reem Island</h2>
-					<h2 class="price">AED 100,000</h2>
-					<div class="row">
-						<div class="col-md-4">
-							<h3 class="specs">1200 sq.ft.</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">4 bedrooms</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">2 bathrooms</h3>
-						</div>
-					</div>
-					<p class="description">
-						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
-					</p>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="card-property">
-					<a href="#"><img src="assets/images/properties/property3-tn.jpg" alt=""></a>
-					<h2 class="title">Hydra Avenue, Reem Island</h2>
-					<h2 class="price">AED 110,000</h2>
-					<div class="row">
-						<div class="col-md-4">
-							<h3 class="specs">1200 sq.ft.</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">4 bedrooms</h3>
-						</div>
-						<div class="col-md-4">
-							<h3 class="specs">2 bathrooms</h3>
-						</div>
-					</div>
-					<p class="description">
-						Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
 
-	<!-- Main Content Ends -->
+		<!-- Featured Locations -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12 headings-main">
 
-	<!-- Footer -->
-	<footer class="container-fluid">
-		<div class="row contact headings-main">
-			<h1>Get in Touch</h1>
-		</div>
-		<div class="row">
-			<div class="col-md-12">
-				<h1>Footer</h1>
+					<h1>Featured Locations</h1>
+				</div>
+				<div class="col-md-3">
+					<div class="featured">
+						<img class="img-responsive" src="assets/images/featured-locations/yas-acres.jpg" alt="Yas Acres, Yas Island, Abu Dhabi">
+						<h2>YAS ACRES</h2>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="featured">
+					<img class="img-responsive" src="assets/images/featured-locations/ansam.jpg" alt="Ansam, Yas Island, Abu Dhabi">
+						<h2>ANSAM</h2>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="featured">
+					<img class="img-responsive" src="assets/images/featured-locations/mayan.jpg" alt="Mayan, Yas Island, Abu Dhabi">
+						<h2>MAYAN</h2>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="featured">
+					<img class="img-responsive" src="assets/images/featured-locations/west-yas.jpg" alt="West Yas, Yas Island, Abu Dhabi">
+						<h2>WEST YAS</h2>
+					</div>
+				</div>
 			</div>
 		</div>
-	</footer>
-	<!-- Footer Ends -->
+		<!-- Featured Locations Ends -->
+
+		<div class="container container-section">
+			<div class="row">
+				<div class="col-md-12 headings-main">
+					<h1>Recent Properties</h1>
+				</div>
+				<div class="col-md-4">
+					<div class="card-property">
+						<a href="#"><img src="assets/images/properties/property1-tn.jpg" alt=""></a>
+						<h2 class="title">Hydra Avenue, Reem Island</h2>
+						<h2 class="price">AED 90,000</h2>
+						<div class="row">
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">1200 sq.ft.</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">4 bedrooms</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">2 bathrooms</h3>
+							</div>
+						</div>
+						<p class="description">
+							Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card-property">
+						<a href="#"><img src="assets/images/properties/property2-tn.jpg" alt=""></a>
+						<h2 class="title">Hydra Avenue, Reem Island</h2>
+						<h2 class="price">AED 100,000</h2>
+						<div class="row">
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">1200 sq.ft.</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">4 bedrooms</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">2 bathrooms</h3>
+							</div>
+						</div>
+						<p class="description">
+							Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card-property">
+						<a href="#"><img src="assets/images/properties/property3-tn.jpg" alt=""></a>
+						<h2 class="title">Hydra Avenue, Reem Island</h2>
+						<h2 class="price">AED 110,000</h2>
+						<div class="row">
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">1200 sq.ft.</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">4 bedrooms</h3>
+							</div>
+							<div class="col-md-4 col-xs-4">
+								<h3 class="specs">2 bathrooms</h3>
+							</div>
+						</div>
+						<p class="description">
+							Hydra Avenue is located on the waterfront in the City of Lights district on Reem Island. Reem Island itself is less than a kilometre away from downtown Abu Dhabi, with multiple bridges connecting it to central Abu Dhabi via Sowwah Island, in addition to the bridge from Salam Street
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<!-- Main Content Ends -->
+
+		<!-- Footer -->
+		<footer class="container-fluid container-section">
+			<div class="row quick-contact headings-main">
+				<h1 class="headings-main">Get in Touch</h1>
+				<div class="col-md-8 col-md-offset-2">
+					<div class="row">
+						<div class="col-md-6">
+							<input class="form-control input-lg" type="text" placeholder="First Name">
+						</div>
+						<div class="col-md-6">
+							<input class="form-control input-lg" type="text" placeholder="Last Name">
+						</div>
+						<div class="col-md-6">
+							<input class="form-control input-lg" type="text" placeholder="Phone">
+						</div>
+						<div class="col-md-6">
+							<input class="form-control input-lg" type="text" placeholder="Email">
+						</div>
+						<div class="col-md-12">
+							<textarea class="form-control" rows="6" placeholder="Message :"></textarea>
+						</div>
+						<div class="col-md-12">
+							<button type="button" class="btn btn-warning btn-lg">Send Message</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h1 class="headings-sub">Social Media</h1>
+					<img class="center-block" src="assets/images/logo_purehome.png" alt="Pure Home Logo">
+					<br>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-facebook"></i>
+					  <i class="fa fa-facebook fa-stack-1x social-icon "></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-twitter"></i>
+					  <i class="fa fa-twitter fa-stack-1x social-icon"></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-whatsapp"></i>
+					  <i class="fa fa-whatsapp fa-stack-1x social-icon"></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-youtube"></i>
+					  <i class="fa fa-youtube fa-stack-1x social-icon"></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-youtube"></i>
+					  <i class="fa fa-google-plus fa-stack-1x social-icon"></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-linkedin"></i>
+					  <i class="fa fa-linkedin fa-stack-1x social-icon"></i>
+					</span>
+					<span class="fa-stack fa-2x">
+					  <i class="fa fa-square fa-stack-2x bg-linkedin"></i>
+					  <i class="fa fa-instagram fa-stack-1x social-icon"></i>
+					</span>
+				</div>
+				<div class="col-md-4">
+					<h1 class="headings-sub"><i class="fa fa-newspaper-o fa-lg"></i> Newsletter</h1>
+					<p>Keep up with the latest company news and events. </p>
+					<p>Enter your e-mail and subscribe to our newsletter.</p>
+					<div class="input-group">
+			      <div class="input-group-addon btn-info"><i class="fa fa-envelope"></i></div>
+			      <input type="text" class="form-control" id="newletter-email" placeholder="Email">
+			      <span class="input-group-btn">
+	        		<button class="btn btn-warning" type="button">SUBSCRIBE</button>
+	      		</span>
+			    </div>
+				</div>
+				<div class="col-md-4">
+					<h1 class="headings-sub">Quick Links</h1>
+					<div class="row">
+						<div class="col-md-6">
+							<ul class="quick-links">
+							<li>Apartments for Sale</li>
+							<li>Apartments for Rent</li>
+							<li>Villas for Sale</li>
+							<li>Villas for Rent</li>
+						</ul>	
+						</div>
+						<div class="col-md-6">
+							<ul class="quick-links">
+								<li>Townhouse for Sale</li>
+								<li>Townhouse for Rent</li>
+								<li>Studio for Sale</li>
+								<li>Studio for Rent</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="copyright-bar col-md-12 text-center">
+					<p>Pure Home Real Estate &copy; 2016 </p>
+				</div>
+			</div>
+		</footer>
+		<!-- Footer Ends -->
 		
 	</body>
 	<script src="assets/js/bootstrap.js"></script>
